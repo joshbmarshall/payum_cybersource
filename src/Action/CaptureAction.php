27d4956ca2489b56aa91b2a1266d9bb9ce38f374
Josh Marshall <josh@jmarshall.com.au>
2024-01-09 15:46:25 +1000

    Process payment

--- src/Action/CaptureAction.php
+++ src/Action/CaptureAction.php
@@ -9,9 +9,11 @@ use Payum\Core\GatewayAwareInterface;
 use Payum\Core\GatewayAwareTrait;
 use Payum\Core\Request\Capture;
 use Cognito\PayumCybersource\Request\Api\ObtainNonce;
+use Cognito\PayumCybersource\Api;
 
-class CaptureAction implements ActionInterface, GatewayAwareInterface {
+class CaptureAction implements ActionInterface, GatewayAwareInterface, \Payum\Core\ApiAwareInterface {
     use GatewayAwareTrait;
+    use \Payum\Core\ApiAwareTrait;
 
     private $config;
 
@@ -20,6 +22,7 @@ class CaptureAction implements ActionInterface, GatewayAwareInterface {
      */
     public function __construct(ArrayObject $config) {
         $this->config = $config;
+        $this->apiClass = Api::class;
     }
 
     /**
@@ -49,31 +52,66 @@ class CaptureAction implements ActionInterface, GatewayAwareInterface {
         if (!$model->offsetExists('status')) {
             // Validate the token from the form
             $token = \Firebase\JWT\JWT::decode(str_replace('"', '', $model['nonce']), \Firebase\JWT\JWK::parseKeySet(['keys' => [$model['public_key']]]), ['RS256']);
-            dump($token);
             // Token ok, attempt to capture funds
 
-            exit;
-                $model['status'] = 'failed';
-                $model['error'] = 'failed';
-            if (false) {
-            $stripe = new \Stripe\StripeClient($this->config['secret_key']);
-            $paymentIntent = $stripe->paymentIntents->retrieve($model['nonce'], []);
-            if ($paymentIntent->status == \Stripe\PaymentIntent::STATUS_SUCCEEDED) {
-                $model['status'] = 'success';
-            } else {
-                // Report error
-                $model['status'] = 'failed';
-                $model['error'] = 'failed';
-            }
-            foreach ($paymentIntent->charges ?? [] as $charge) {
-                $model['transactionReference'] = $charge->id;
-                $model['result'] = $charge;
-            }
-            if ($paymentIntent->latest_charge ?? false) {
-                $model['transactionReference'] = $paymentIntent->latest_charge;
-                $charge = $stripe->charges->retrieve($paymentIntent->latest_charge);
-                $model['stripe_charge_info'] = $charge;
-            }
+            $config = new \CyberSource\Configuration();
+            $config->setHost(str_replace('https://', '', $this->api->getApiEndpoint()));
+
+            $merchantConfig = new \CyberSource\Authentication\Core\MerchantConfiguration();
+            $merchantConfig->setMerchantID($model['merchant_id']);
+            $merchantConfig->setApiKeyID($model['access_key']);
+            $merchantConfig->setSecretKey($model['secret_key']);
+            $merchantConfig->setAuthenticationType('HTTP_SIGNATURE');
+            $merchantConfig->setUseMetaKey(false);
+
+            $api_client = new \CyberSource\ApiClient($config, $merchantConfig);
+            $api_instance = new \CyberSource\Api\PaymentsApi($api_client);
+
+            $model['status'] = 'failed';
+            $model['error']  = '';
+            $model['transactionReference'] = '';
+            $status = '';
+            try {
+                $apiResponse = $api_instance->createPayment(new \CyberSource\Model\CreatePaymentRequest([
+                    'clientReferenceInformation' => new \CyberSource\Model\Ptsv2paymentsClientReferenceInformation([
+                                'code' => $model['payment_reference'],
+                            ]),
+                    'orderInformation' => new \CyberSource\Model\Ptsv2paymentsOrderInformation([
+                            'amountDetails' => new \CyberSource\Model\Ptsv2paymentsOrderInformationAmountDetails([
+                                    'totalAmount' => $model['amount'],
+                                    'currency' => $model['currency']
+                                ]),
+                            'billTo' => new \CyberSource\Model\Ptsv2paymentsOrderInformationBillTo([
+                                    'firstName' => $model['billing']['first_name'],
+                                    'lastName' => $model['billing']['last_name'],
+                                    'address1' => $model['billing']['address']['line1'],
+                                    'address2' => $model['billing']['address']['line2'],
+                                    'locality' => $model['billing']['address']['city'],
+                                    'administrativeArea' => $model['billing']['address']['state'],
+                                    'postalCode' => $model['billing']['address']['postal_code'],
+                                    'country' => $model['billing']['address']['country'],
+                                    'email' => $model['billing']['email'],
+                                    //'phoneNumber' => $model['billing']['phone'],
+                                ]),
+                            ]),
+                    'tokenInformation' => new \CyberSource\Model\Ptsv2paymentsTokenInformation([
+                                'transientTokenJwt' => $model['nonce']
+                            ]),
+                    'processingInformation' => new \CyberSource\Model\Ptsv2paymentsProcessingInformation([
+                                'capture' => true,
+                            ]),
+                ]));
+
+                $status = $apiResponse[0]->getStatus();
+                if ($status == 'AUTHORIZED') {
+                    $model['status'] = 'success';
+                } else {
+                    $model['error'] = $status . ' ' . $apiResponse[0]->getErrorInformation()->getMessage();
+                }
+                $model['transactionReference'] = $apiResponse[0]->getId();
+                $model['result'] = serialize($apiResponse[0]);
+            } catch (\Cybersource\ApiException $e) {
+                $model['error'] = $e->getResponseBody()->message . ' ' . json_encode($e->getResponseBody()->details);
             }
         }
     }
